57658e3975c728cfdbb85d16d936274b29dc7432
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-11-11 17:03:51 +0300

    Our Leader

--- client/src/pages/blog/components/blog-details/components/likes-views/index.jsx
+++ client/src/pages/blog/components/blog-details/components/likes-views/index.jsx
@@ -7,6 +7,7 @@ import {useBlogs} from "../../../../../../store/hooks/hooks";
 export default function LikesViews({data, incremenetLikes, blogID}) {
 
     const currentLikes = data.blogs?.likes_count || 0;
+    const currentViews = data.blogs?.view_count || 0
 
     const handleLike = () => {
         incremenetLikes(blogID, currentLikes + 1, currentViews);
@@ -19,7 +20,7 @@ export default function LikesViews({data, incremenetLikes, blogID}) {
                 <AiFillLike className="text-gray-600 hover:text-blue-700 text-lg mr-1"/>
                 <span className="font-semibold">{currentLikes}</span>
             </span>
-            <span onClick={handleView} className="flex items-center text-gray-600">
+            <span  className="flex items-center text-gray-600">
                 <FaEye className="w-5 h-5 mr-1"/>
                 <span className="font-semibold">Views</span>
             </span>
